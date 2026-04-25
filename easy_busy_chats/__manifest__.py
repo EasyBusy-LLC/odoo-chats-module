b9a52f0eb2b20d072ef39cd4a9936256c4858c99
Oleksandr Bereza <oleksandr.bereza@easy-busy.cloud>
2026-04-25 20:03:27 +0000

    Add keywords
    GitLab: https://gitlab.dev-ops.top/easy-busy-crm/chats-integrations/-/commit/2f9ebea04b084542908c5b6672c8f1c5a47f16ad

--- easy_busy_chats/__manifest__.py
+++ easy_busy_chats/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '17.0.1.3.0',
     'category': 'Services/Chats',
     'summary': 'Unified messenger inbox in Odoo with contact linking and chat history.',
+    'keywords': ['telegram', 'instagram', 'whatsapp', 'viber', 'webchat', 'messenger', 'facebook'],
     'images': ['static/description/banner.png'],
     'description': """
 EasyBusy Chats for Odoo
